e217601288d5e9c83180781436ea381785dd6773
ButterKuma <42940018+ButterKuma@users.noreply.github.com>
2018-09-04 04:04:25 +0900

    first Upload

---
+++ Assets/Script/StartManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void GameEnd() {
+        Debug.Log("게임을 종료합니다.");
+        Application.Quit();
+    }
+}

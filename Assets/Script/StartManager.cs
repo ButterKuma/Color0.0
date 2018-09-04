2e5aaca4ec48a9b7c5ccb0d9c980ec41b9d4a4fb
ButterKuma <42940018+ButterKuma@users.noreply.github.com>
2018-09-04 16:22:17 +0900

    0904_0

--- Assets/Script/StartManager.cs
+++ Assets/Script/StartManager.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartManager : MonoBehaviour {
-
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
+
+    }
+    public void GameStart() {
+        int id = 0; // 임시값 0
+        string name = null;
+
+        Debug.Log("게임을 새로 생성합니다.");
+
+        playerData data = new playerData(name,id);
+    }
     public void GameEnd() {
         Debug.Log("게임을 종료합니다.");
         Application.Quit();
+    }
+
+    public void LoadGame() {
+
     }
 }

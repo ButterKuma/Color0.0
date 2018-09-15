fb29cd013024f704df9d58fe51da1d1ae0612300
ButterKuma <42940018+ButterKuma@users.noreply.github.com>
2018-09-15 14:20:17 +0900

    0915

---
+++ Assets/Script/PlayerDataManagement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerDataManagement : MonoBehaviour {
+    public static PlayerDataManagement SingletonInstance = null;
+
+    public static PlayerDataManagement Singleton() {
+
+        if (!SingletonInstance)
+        {
+            GameObject TempSinggletonInstance = new GameObject();
+            SingletonInstance = TempSinggletonInstance.AddComponent<PlayerDataManagement>();
+
+            DontDestroyOnLoad(SingletonInstance);
+        }
+
+        return SingletonInstance;
+    }
+    public PlayerData NowPlayerData = new PlayerData();
+    
+    void Start()
+    {
+        
+    }
+
+    void Update() {
+
+    }
+
+}
+//player Data Struct Class
+[System.Serializable]
+public class PlayerData
+{
+
+public int ID;
+public string Name;
+public int Level;
+public int Health;
+public int Damage;
+public int Defence;
+public int Total_EXP;
+public int P_EXP;
+public int Money;
+public List<int> DoneQuest = new List<int>(); //진행 중 퀘스트
+public List<int> FinishQuest = new List<int>(); //완료한 퀘스트
+public List<int> Inventory = new List<int>(); //아이템 Inventory
+public List<int> S_Position = new List<int>(); //마지막 저장 위치 0 = x, 1 = y, 2 = z, 
+}
--- Assets/Script/StartManager.cs
+++ Assets/Script/StartManager.cs
@@ -20,8 +20,8 @@ public class StartManager : MonoBehaviour
     // Use this for initialization
     void Awake()
     {
-        ScrollView.SetActive(false);
         SetName = GameObject.Find("InputField").GetComponent<InputField>();
+        ScrollView.SetActive(false);
         PanelSetName.SetActive(false);
     }
     private void Update()
@@ -33,12 +33,14 @@ public class StartManager : MonoBehaviour
         Debug.Log("게임을 새로 생성합니다.");
         PanelSetName.SetActive(true);
         panelRectTransform.SetAsFirstSibling();
+
     }
     public void OnClickLoadButton()
     {
-        StartCoroutine(LoadCo());
+        StartCoroutine(LoadCo(1));
         ScrollView.SetActive(true);
-        ScrollVeiwTransform.SetAsFirstSibling();
+        ScrollVeiwTransform.SetAsLastSibling();
+
     }
     public void OnClickEndButton()
     {
@@ -47,21 +49,47 @@ public class StartManager : MonoBehaviour
         Application.Quit();
     }
 
-    IEnumerator LoadCo()
+    IEnumerator LoadCo(int select)
     {
         string Jsonstring = File.ReadAllText(Application.dataPath + "/Resources/Data/PlayerData.json");
 
         JsonData PlayerList = JsonMapper.ToObject(Jsonstring);
+        Debug.Log(Jsonstring);
+
         yield return null;
     }
     public void OnClickCancle()
     {
         PanelSetName.SetActive(false);
+        ScrollView.SetActive(false);
         panelRectTransform.SetAsLastSibling();
+        ScrollVeiwTransform.SetAsLastSibling();
     }
     public void OnClickOk()
     {
-
-        Debug.Log(SetName.text);
+        int id = 2;
+        InitializeData(SetName.text, id);
     }
+    public void onClickItemView() {
+        Debug.Log("OK");
+    }
+
+    public void InitializeData(string name, int id)
+    {
+        Debug.Log(id + name); //체크용
+        PlayerDataManagement.Singleton().NowPlayerData.ID = id;
+        PlayerDataManagement.Singleton().NowPlayerData.Name = name;
+        PlayerDataManagement.Singleton().NowPlayerData.Level = 1;
+        PlayerDataManagement.Singleton().NowPlayerData.Health = 100;
+        PlayerDataManagement.Singleton().NowPlayerData.Damage = 10;
+        PlayerDataManagement.Singleton().NowPlayerData.Defence = 15;
+        PlayerDataManagement.Singleton().NowPlayerData.Total_EXP = 400;
+        PlayerDataManagement.Singleton().NowPlayerData.P_EXP = 0;
+        PlayerDataManagement.Singleton().NowPlayerData.Money = 0;
+        PlayerDataManagement.Singleton().NowPlayerData.DoneQuest.Clear();
+        PlayerDataManagement.Singleton().NowPlayerData.FinishQuest.Clear();
+        PlayerDataManagement.Singleton().NowPlayerData.Inventory.Clear();
+        PlayerDataManagement.Singleton().NowPlayerData.S_Position.Clear();
+    }
+
 }
